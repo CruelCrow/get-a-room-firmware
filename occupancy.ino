2d47a06691f8d5a19f42e48c7824cf01293247f7
michael.kalenkovich <michael.kalenkovich@autodesk.com>
2018-12-24 15:44:32 +0200

    initial commit

---
+++ occupancy.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include "publicConfiguration.h"
+#include "privateConfiguration.h"
+
+bool isHumanPresentStatistics[PIR_STATUS_PROBE_FREQUENCY];
+HTTPClient http;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println();
+  
+  pinMode(ESP8266_LED, OUTPUT);
+  pinMode(PIR_MODE, OUTPUT);
+  pinMode(PIR_STATUS, INPUT);
+
+  setupWiFi();
+  setupPir();
+}
+
+void setupWiFi()
+{
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+  
+  if (WIFI_MODE == WIFI_MODE_NO_AUTH || WIFI_MODE == WIFI_MODE_WEB_AUTH)
+  {
+    WiFi.begin(WIFI_SSID);
+  } else if (WIFI_MODE == WIFI_MODE_PASS_AUTH)
+  {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  }
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupPir() {
+  digitalWrite(PIR_MODE, PIR_MODE_REPEATABLE);
+  
+  memset(isHumanPresentStatistics, 0, sizeof(isHumanPresentStatistics));
+}
+
+unsigned short currentItteration = 0;
+void loop()
+{
+  bool isPresentNow = digitalRead(PIR_STATUS);
+  isHumanPresentStatistics[currentItteration] = isPresentNow;
+  Serial.print(isPresentNow ? "!" : ".");
+  delay(1000 / PIR_STATUS_PROBE_FREQUENCY);
+  currentItteration = ++currentItteration % PIR_STATUS_PROBE_FREQUENCY;
+  if (currentItteration == 0) {
+    reportToServer();
+    memset(isHumanPresentStatistics, 0, sizeof(isHumanPresentStatistics));
+  }
+
+}
+
+void reportToServer()
+{
+  bool _isHumanPresent = isHumanPresent();
+  
+  Serial.print("Is human present: ");
+  Serial.print(_isHumanPresent ? "Yes" : "No");
+  Serial.println();
+  digitalWrite(ESP8266_LED, _isHumanPresent);
+
+  Serial.print("Reporting to server");
+  Serial.println();
+
+  http.begin("http://jsonplaceholder.typicode.com/users/1");
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println(httpCode, DEC);
+    Serial.println(payload);
+
+  }
+  http.end();
+}
+
+bool isHumanPresent()
+{
+  int presentCount = 0;
+  for (int i = 0; i < PIR_STATUS_PROBE_FREQUENCY; i++) {
+    if (isHumanPresentStatistics[i] == true) {
+      ++presentCount;
+    }
+  }
+
+  Serial.print("presentCount: ");
+  Serial.print(presentCount, DEC);
+  Serial.print(" out of : ");
+  Serial.print(PIR_STATUS_PROBE_FREQUENCY, DEC);
+  Serial.println();
+  
+  int percentage = (presentCount * 100) / PIR_STATUS_PROBE_FREQUENCY;
+  Serial.print("Presence percentage: ");
+  Serial.print(percentage, DEC);
+  Serial.println();
+  return percentage >= PRESENCE_STATISTICS_PERCENTAGE_FOR_TRUE;
+}
---
+++ publicConfiguration.h
@@ -0,0 +1,22 @@
+#define ESP8266_LED 5
+#define PIR_MODE 0
+#define PIR_STATUS 4
+
+#define PIR_MODE_REPEATABLE LOW
+#define PIR_MODE_NON_REPEATABLE HIGH
+
+#define PIR_STATUS_PROBE_FREQUENCY 50
+#define PRESENCE_STATISTICS_PERCENTAGE_FOR_TRUE 10
+
+#define API_SERVER "bazinga-get-a-room.herokuapp.com"
+#define API_URI "/api/report"
+#define API_KEY "your-api-key-is-here"
+#define API_SENSOR_ID "your-room-id-is-here
+
+#define WIFI_MODE_NO_AUTH 0
+#define WIFI_MODE_PASS_AUTH 1
+#define WIFI_MODE_WEB_AUTH 2
+
+#define WIFI_MODE WIFI_MODE_PASS_AUTH
+#define WIFI_SSID "CatsFi"
+#define WIFI_PASSWORD "siwh3818"
